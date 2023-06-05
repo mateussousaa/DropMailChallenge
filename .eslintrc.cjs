0ddca1a1a6b8cc4567a9c8a5db61556b20507dcf
mateussousaa <mateussousa20@outlook.com>
2023-06-05 18:47:11 -0300

    build: initial setup with eslint plugins and tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,33 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'airbnb',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', 'import', '@typescript-eslint', 'react-hooks', 'jsx-a11y'],
   rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/vite.config.ts', // jest setup
+        ],
+        optionalDependencies: false,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
   },
-}
+};
